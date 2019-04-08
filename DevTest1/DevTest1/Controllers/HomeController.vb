aad8966440dac502f21b23aedd9e09d5f76d6fb9
DESKTOP-CJ76F6P\khana <khanal.raj124@gmail.com>
2019-04-08 11:56:33 -0500

    Final Commit
    Used StringBuilder instead of string to append strings because when we use string type, for every string concatenation a new string object is created in memory and the old string is destroyed. This is a costly operation. String are immutable which means a string once created cannot be altered in memory. A copy of it is created with the new changes.

---
+++ DevTest1/DevTest1/Controllers/HomeController.vb
@@ -0,0 +1,31 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+
+        Return View()
+
+    End Function
+
+    Function CsvReport() As ActionResult
+
+        Dim list As List(Of Customer) = Customer.CreateList()
+
+        'Using StringBuilder instead of String
+        Dim csvHeader As StringBuilder = New StringBuilder()
+        csvHeader.Append("Company, Name, EmailAddress" & vbCr)
+        Dim csvBody As StringBuilder = New StringBuilder()
+
+        'build body
+        For Each c As Customer In list
+            csvBody.Append(c.Company & ", " & c.Name + ", " + c.EmailAddress & vbCr)
+        Next
+
+        ViewBag.CsvReport = csvHeader.Append(csvBody)
+
+
+        Return View()
+
+    End Function
+
+End Class
